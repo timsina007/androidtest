57a5794c8f7dd29da0e9492fb0838d5f0ba9cd9a
timsina007 <timsina007@gannon.edu>
2023-02-20 14:41:38 -0500

    service

---
+++ ServiceApplication/app/src/main/java/com/example/serviceapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.serviceapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void playMusic(View view) {
+    }
+
+    public void stopMusic(View view) {
+    }
+}
