beaeb6b8e87a67cbe36bb96668915504be724353
timsina007 <timsina007@gannon.edu>
2023-02-20 14:48:36 -0500

    Update MusicPlayerService.java

--- ServiceApplication/app/src/main/java/com/example/serviceapplication/MusicPlayerService.java
+++ ServiceApplication/app/src/main/java/com/example/serviceapplication/MusicPlayerService.java
@@ -3,14 +3,33 @@ package com.example.serviceapplication;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.media.MediaPlayer;
+import android.provider.Settings;
+import android.widget.Toast;
+
 
 public class MusicPlayerService extends Service {
+
+    MediaPlayer mPlayer;
+
     public MusicPlayerService() {
     }
 
     @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Toast.makeText(this, "Music Player Service Started",
+                Toast.LENGTH_SHORT).show();
+
+        mPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+        mPlayer.setLooping(true);
+        mPlayer.start();
+
+        return START_STICKY;
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 }
